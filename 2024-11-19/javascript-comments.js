b08ee5004b3d2a566821b003cad05c5778debf63
Christopher Hoffmann <cstephanhoffmann@gmail.com>
2024-11-20 12:59:34 -0500

    nov 19 weekly challenge

---
+++ 2024-11-19/javascript-comments.js
@@ -0,0 +1,13 @@
+// In JavaScript, there are two types of comments:
+
+// Single-line comments start with //
+
+// Multi-line or inline comments start with /* and end with */
+
+// The input will be a sequence of //, /* and */. Every /* must have a */ that immediately follows it. To add, there can be no single-line comments in between multi-line comments in between the /* and */.
+
+// Create a function that returns True if comments are properly formatted, and False otherwise.
+
+function commentsCorrect(input) {
+  return false;
+}
